391217de0fd81a7508c1ed87bf1cfa5173021ee5
Yashdeep Prasad <45161513+HackGod2000@users.noreply.github.com>
2021-11-16 05:41:03 +0530

    Structured ALT-R Website

---
+++ Website/src/App.js
@@ -0,0 +1,29 @@
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+	return (
+		<Container
+			className="d-flex align-items-center justify-content-center"
+			style={{ minHeight: "100vh" }}
+		>
+			<div className="w-100" style={{ maxWidth: "400px" }}>
+				<Router>
+					<AuthProvider>
+						<Routes>
+							<Route exact path="/" element={<Dashboard />} />
+							<Route exact path="/signup" element={<Signup />} />
+							<Route exact path="/login" element={<Login />} />
+						</Routes>
+					</AuthProvider>
+				</Router>
+			</div>
+		</Container>
+	);
+}
+
+export default App;
